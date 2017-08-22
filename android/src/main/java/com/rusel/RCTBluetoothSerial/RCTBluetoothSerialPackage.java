a7433deb89743f3850c0a13ce1d519e1a71a3e5e
Ali Rostami <kptlronyttcna@gmail.com>
2017-08-22 11:36:15 +0430

    Update to RN 0.47

--- android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
+++ android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
@@ -20,7 +20,8 @@ public class RCTBluetoothSerialPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
